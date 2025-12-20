bb59c43f979a88c6599f4ebdd9ce0a30df087f20
Eunchong Yu <kroisse@gmail.com>
2025-12-21 08:56:16 +0900

    refactor: parse salsa_test with unsynn

--- crates/salsa-test-macros/src/lib.rs
+++ crates/salsa-test-macros/src/lib.rs
@@ -1,84 +1,108 @@
-use proc_macro::TokenStream;
-use quote::quote;
-use unsynn::{EndOfStream, Group, Parser, ToTokens, TokenStream as UnsTokenStream};
+use proc_macro::TokenStream as ProcTokenStream;
+use quote::{format_ident, quote};
+use unsynn::{EndOfStream, Group, Parser, ToTokens, TokenStream, unsynn};
+
+unsynn! {
+    use unsynn::combinator::Cons;
+    use unsynn::Colon;
+    use unsynn::{
+        BraceGroup, GroupContaining, Ident, LazyVecUntil, NonEmptyOption, TokenTree,
+    };
+
+    keyword Mut = "mut";
+
+    enum ParamName {
+        Mut(Cons<Mut, Ident>),
+        Plain(Ident),
+    }
+
+    struct Param {
+        name: ParamName,
+        colon: Colon,
+        ty: TokenStream,
+    }
+
+    type ParamsGroup = GroupContaining<NonEmptyOption<Param>>;
+
+    struct SalsaTestFn {
+        head: LazyVecUntil<TokenTree, ParamsGroup>,
+        params: ParamsGroup,
+        tail: LazyVecUntil<TokenTree, BraceGroup>,
+        body: BraceGroup,
+    }
+}
 
 #[proc_macro_attribute]
-pub fn salsa_test(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn salsa_test(attr: ProcTokenStream, item: ProcTokenStream) -> ProcTokenStream {
     if let Err(err) = parse_empty_attr(attr) {
         return compile_error(&err.to_string());
     }
 
-    let input = UnsTokenStream::from(item);
+    let input = TokenStream::from(item);
     match transform_fn(input) {
         Ok(tokens) => tokens.into(),
         Err(err) => compile_error(&err.to_string()),
     }
 }
 
-fn parse_empty_attr(attr: TokenStream) -> Result<(), Box<unsynn::Error>> {
-    let mut tokens = UnsTokenStream::from(attr).to_token_iter();
-    EndOfStream::parser(&mut tokens)
-        .map(|_| ())
-        .map_err(Box::new)
+fn parse_empty_attr(attr: ProcTokenStream) -> Result<(), Box<unsynn::Error>> {
+    let mut tokens = TokenStream::from(attr).to_token_iter();
+    EndOfStream::parser(&mut tokens)?;
+    Ok(())
 }
 
-fn transform_fn(input: UnsTokenStream) -> Result<UnsTokenStream, Box<unsynn::Error>> {
+fn transform_fn(input: TokenStream) -> Result<TokenStream, Box<unsynn::Error>> {
     let mut tokens = input.to_token_iter();
-    let func: unsynn::TokenStream = unsynn::TokenStream::parser(&mut tokens).map_err(Box::new)?;
-    let mut func_tokens: Vec<unsynn::TokenTree> = func.into_iter().collect();
+    let func: SalsaTestFn = SalsaTestFn::parser(&mut tokens).map_err(Box::new)?;
+    let db_ident = db_ident_from_params(&func.params)?;
+    let body_group: Group = func.body.into();
 
-    let body_index = func_tokens.iter().rposition(|token| {
-        matches!(token, unsynn::TokenTree::Group(group) if group.delimiter() == unsynn::Delimiter::Brace)
-    });
-    let Some(body_index) = body_index else {
-        let empty = UnsTokenStream::new().to_token_iter();
-        return unsynn::Error::other::<UnsTokenStream>(
-            None,
-            &empty,
-            "salsa_test expects a function with a brace body".to_string(),
-        )
-        .map_err(Box::new);
-    };
-
-    let params_index = func_tokens[..body_index].iter().rposition(|token| {
-        matches!(token, unsynn::TokenTree::Group(group) if group.delimiter() == unsynn::Delimiter::Parenthesis)
-    });
-    let Some(params_index) = params_index else {
-        let empty = UnsTokenStream::new().to_token_iter();
-        return unsynn::Error::other::<UnsTokenStream>(
-            None,
-            &empty,
-            "salsa_test expects a function with a parameter list".to_string(),
-        )
-        .map_err(Box::new);
-    };
-
-    func_tokens[params_index] = unsynn::TokenTree::Group(Group::new(
-        unsynn::Delimiter::Parenthesis,
-        UnsTokenStream::new(),
-    ));
-
-    let body_group = match func_tokens.remove(body_index) {
-        unsynn::TokenTree::Group(group) => group,
-        _ => unreachable!("body_index points to a group"),
-    };
     let wrapped_body = quote!({
-        salsa::Database::attach(&salsa::DatabaseImpl::default(), |db| {
+        salsa::Database::attach(&salsa::DatabaseImpl::default(), |#db_ident| {
             #body_group
         });
     });
 
-    func_tokens.insert(
-        body_index,
-        unsynn::TokenTree::Group(Group::new(unsynn::Delimiter::Brace, wrapped_body)),
-    );
-
-    let mut output = UnsTokenStream::new();
+    let mut output = TokenStream::new();
     output.extend(quote!(#[test]));
-    output.extend(func_tokens);
+    func.head.to_tokens(&mut output);
+    output.extend(quote!(()));
+    func.tail.to_tokens(&mut output);
+    output.extend(wrapped_body);
     Ok(output)
 }
 
-fn compile_error(message: &str) -> TokenStream {
+impl ParamName {
+    fn ident(&self) -> &unsynn::Ident {
+        match self {
+            ParamName::Mut(cons) => &cons.second,
+            ParamName::Plain(ident) => ident,
+        }
+    }
+}
+
+fn db_ident_from_params(params: &ParamsGroup) -> Result<proc_macro2::Ident, Box<unsynn::Error>> {
+    match &params.content.0 {
+        None => Ok(format_ident!("db")),
+        Some(param) => {
+            let ident = param.name.ident();
+            if ident == "_" {
+                return other_error("salsa_test expects a named parameter, not `_`");
+            }
+            Ok(ident.clone())
+        }
+    }
+}
+
+fn other_error<T>(message: &str) -> Result<T, Box<unsynn::Error>> {
+    let empty = TokenStream::new().to_token_iter();
+    Ok(unsynn::Error::other::<T>(
+        None,
+        &empty,
+        message.to_string(),
+    )?)
+}
+
+fn compile_error(message: &str) -> ProcTokenStream {
     quote!(compile_error!(#message);).into()
 }
